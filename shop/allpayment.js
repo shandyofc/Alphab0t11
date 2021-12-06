a13464f62fe25a51d6432e0ed1c737d3dc7fa23c
shandyofc <95405547+shandyofc@users.noreply.github.com>
2021-12-07 05:51:36 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,17 +1,6 @@
 const allpayment = (prefix) => {
 return `PAYMENT BY SHANDYONE OFC
 
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
-	
 *💰 E-MONEY*
 
 1. GOPAY
